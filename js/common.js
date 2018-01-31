afc78c60e4bf73a3f5b9dba2165eb9d8839f54bb
SeungheeShin <doublesandh@gmail.com>
2018-01-31 14:27:40 +0900

    main slide box [1] css Attribute declaration order [2] slide box content write

--- js/common.js
+++ js/common.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     var $menuList = $('.box-header-menu li a, .box-header-etc li button, .box-header-submenu li a');
 
     //화면 스크롤 메뉴 포커스 효과
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(this).scrollTop() >= 20) {
             $boxHeader.addClass('active');
             $imgLogo.addClass('active');
@@ -71,6 +71,11 @@ $(document).ready(function () {
     });
 
     //box-slide
+    $('.box-slide > div').css('width', $(window).width());
+    $(window).resize(function () { // 이거 왜 작동 안하지?
+        $('.box-slide > div').css('width', $(window).width());
+    }).resize();
+
     var ltem_width = $('.box-slide > div').outerWidth(true); //슬라이드될 박스 너비값
     $('.box-slide > div:first').before($('.box-slide > div:last')); //맨 마지막 슬라이드될 박스를 맨 앞으로 이동시켜둠
     $('.box-slide').css('left', -ltem_width + 'px'); //늘어난 만큼 -로 위치 설정
@@ -78,23 +83,22 @@ $(document).ready(function () {
     $('.btn-prev').click(function () {
         var left_indent = parseInt($('.box-slide').css('left')) + ltem_width;
 
-        $('.box-slide').animate({'left':left_indent+'px'}, 200, function () {
+        $('.box-slide').animate({'left': left_indent + 'px'}, 200, function () {
             $('.box-slide > div:first').before($('.box-slide > div:last'));
             // $('.box-slide > div:last').insertBefore($('.box-slide > div:first'));
-            $('.box-slide').css('left', -ltem_width +'px');
+            $('.box-slide').css('left', -ltem_width + 'px');
         });
     });
 
     $('.btn-next').click(function () {
         var left_indent = parseInt($('.box-slide').css('left')) - ltem_width;
 
-        $('.box-slide').animate({'left':left_indent+'px'}, 200, function () {
+        $('.box-slide').animate({'left': left_indent + 'px'}, 200, function () {
             $('.box-slide > div:last').after($('.box-slide > div:first'));
             // $('.box-slide > div:first').insertAfter($('.box-slide > div:last'));
-            $('.box-slide').css('left', -ltem_width +'px');
+            $('.box-slide').css('left', -ltem_width + 'px');
         });
     });
-
 
 
 });
